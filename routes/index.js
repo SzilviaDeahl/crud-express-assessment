69e58b6d759796804c87606aa5f7dc6a0a4e7078
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-07-23 16:32:35 -0600

    express-crud app created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')(process.env.MONGOLAB_URI || 'localhost/mydb');
+var users = db.get('users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  users.find({}, function (err, record) {
+  res.render('index', { articles: record });
+  })
 });
+
+router.get('/show', function (req, res, next) {
+  res.render('show')
+});
+
+router.post('/show', function (req, res, next) {
+  // if (checkbox===checked){
+  //   title.style.color = white;
+  // }
+  res.render('show')
+});
+
+
 
 module.exports = router;
